942ef14e217be0ac2fe171a8db46fbc32118f991
irBug <irwingalvarez@gmail.com>
2022-11-07 16:33:53 -0600

    need help figuring out why my client.query is returning undefined

--- server.js
+++ server.js
@@ -9,23 +9,27 @@ app.use(morgan('dev'));
 
 app.use(express.static('public'));
 
-app.get('/', async (req, res, next) => {
-  const retroGamingConsoles = await client.query('SELECT * FROM retroconsoles;')
-    .rows;
-  console.log(retroGamingConsoles); //I would appreciate some help figuring out why this returns undefined.
-  res.send(homePage(retroGamingConsoles));
+app.get('/', async (req, res) => {
+  try {
+    const retroGamingConsoles = await client.query(
+      'SELECT * FROM retroconsoles;'
+    ).rows; //This should return an array of "row objects", where every object has a property corresponding to a column in that row (i.e {name,abbrev,releaseYear,numberOfPlayers})
+    console.log(retroGamingConsoles); //I would appreciate some help figuring out why this returns undefined.
+    res.send(homePage(retroGamingConsoles)); //pass the array of game console objects, retroGamingConsoles, to the homePage function which returns html that uses the array.
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-// app.get('/:abbrev', async (req, res, next) => {
-//   const foundConsole = await client.query(
-//     `SELECT * FROM retroconsoles WHERE abbrev = '${req.params.abbrev}';`
-//   ).rows[0];
-
-//   if (foundConsole) {
-//     res.send(details(foundConsole));
-//   } else {
-//     res.send(pageNotFound());
-//   }
-// });
+app.get('/:abbrev', async (req, res) => {
+  try {
+    const foundConsole = await client.query(
+      `SELECT * FROM retroconsoles WHERE abbrev = '${req.params.abbrev}';`
+    ).rows[0];
+    res.send(details(foundConsole));
+  } catch {
+    res.send(pageNotFound());
+  }
+});
 
 app.listen(port, console.log(`Listening on port ${port}`));
