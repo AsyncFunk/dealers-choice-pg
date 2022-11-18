73e46ecf3b5a0a567270217cdf4b43e1f194fce7
Jung Yeon Choi <water@Peters-MBP.monkeybrains.net>
2022-11-17 17:44:43 -0800

    asdf

--- views/index.js
+++
@@ -1,5 +0,0 @@
-const pageNotFound = require('./pageNotFound');
-const homePage = require('./homePage');
-const details = require('./details');
-
-module.exports = { pageNotFound, homePage, details };
